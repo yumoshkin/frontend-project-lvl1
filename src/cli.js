32364679e8b6efd81f333081ba33c60633155a57
Yury Moshkin <yumoshkin@gmail.com>
2021-08-18 17:59:09 +0300

    add lint

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greet = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.info(`Hello, ${name}!`);
+};
+
+export { greet };
